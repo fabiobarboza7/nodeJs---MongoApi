88ad794d391b94fda30e23d5015d3062b1c43c70
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-05-14 19:17:31 -0300

    Learning MongoDb, creating a simple api

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir')
 
+
+// iniciando app
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('ass');
-});
+// iniciando db
+mongoose.connect(
+	'mongodb://localhost:27017/nodeapi',
+	{ useNewUrlParser: true }
+);
+requireDir('./src/models');
+
+
+
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
